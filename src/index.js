1682d72e5fda8e486efeddf7d07f333554710541
Paulo Henrique <phvcandido@gmail.com>
2023-04-29 17:56:41 -0300

    Add scripts to automated install common packages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const chalk = require("chalk");
 const figlet = require("figlet");
 const inquirer = require("inquirer");
 
+const packagesDependencies = require("./util/packageDependencies");
+
 const package = require("../package.json");
 
 program.version(package.version);
@@ -131,6 +133,105 @@ program.command("generate [entities...]").action(async (cEntities) => {
 
   // remove a pasta temp da lib
   shell.rm("-rf", `${libPath}temp`);
+});
+
+const successMessage = (message) => console.log(`${chalk.green(message)}`);
+// generate os arquivos plop para toda uma entidade
+// gerando os hooks, services, pages e components para a entidade passada
+program.command("install [entities...]").action(async (cEntities) => {
+  // localização da pasta do usuário
+  const userPath = shell.pwd().stdout;
+  console.log(userPath);
+  let entities = cEntities;
+  if (!entities?.length) {
+    entities = (
+      await inquirer.prompt([
+        {
+          type: "input",
+          name: "entities",
+          message: "Quais entidades a serem geradas?",
+          validate: (value) => (value ? true : "Precisa especificar entidades"),
+        },
+      ])
+    )?.entities
+      .trimEnd()
+      .trimStart()
+      .split(" ");
+  }
+
+  shell.cd(userPath);
+
+  // verifica se o package.json existe na pasta do usuário
+  // caso não exista, pergunta ao usuário se pode criar com yarn
+  if (!shell.test("-f", `${userPath}/package.json`)) {
+    const { createPackageJson, useYarn, addGitIgnore, initGitRepository } =
+      await inquirer.prompt([
+        {
+          type: "confirm",
+          name: "createPackageJson",
+          message: "Não foi encontrado o package.json, deseja criar?",
+        },
+        {
+          type: "confirm",
+          name: "useYarn",
+          message: "Deseja utilizar yarn?",
+        },
+        {
+          type: "confirm",
+          name: "initGitRepository",
+          message:
+            "Se não estiver configurado, deseja inicializar o repositório git?",
+        },
+        {
+          type: "confirm",
+          name: "addGitIgnore",
+          message: "Deseja adicionar o .gitignore?",
+        },
+      ]);
+
+    // cria o package.json com yarn ou npm
+    if (createPackageJson) {
+      if (useYarn) {
+        shell.exec("yarn init -y");
+      } else {
+        shell.exec("npm init -y");
+      }
+    }
+
+    successMessage("Configuração de package json adicionada!");
+
+    // adiciona o .gitignore fazendo um download do github para a configuração de node.js
+    if (addGitIgnore) {
+      shell.exec(
+        "curl -o .gitignore https://raw.githubusercontent.com/github/gitignore/master/Node.gitignore"
+      );
+    }
+
+    successMessage("Gitignore adicionado!");
+
+    // inicializa o repositório git
+    if (initGitRepository) {
+      shell.exec("git init");
+    }
+
+    successMessage("Repositório git inicializado!");
+  }
+
+  let packagesToBeInstalled = [];
+  // executa o comando para cada entrada de services
+  entities.forEach((entity) => {
+    packagesToBeInstalled = [
+      ...packagesToBeInstalled,
+      ...packagesDependencies[entity],
+    ];
+  });
+
+  // roda o install via yarn ou npm dos packagesToBeInstalled
+  shell.exec(`yarn add ${packagesToBeInstalled.join(" ")}`);
+
+  successMessage(
+    `O(s) pacote(s) ${packagesToBeInstalled.join(",")} foram adicionados!`
+  );
 });
 
 program.parse(process.argv);
---
+++ src/util/packageDependencies.js
@@ -0,0 +1,19 @@
+const packagesDependencies = {
+  "redux-toolkit": ["@reduxjs/toolkit", "react-redux"],
+  // material ui v5
+  mui: ["@mui/material", "@emotion/react", "@emotion/styled"],
+  // material ui v4
+  "material-ui": ["@material-ui/core", "@material-ui/icons"],
+  // styled components
+  styled: ["styled-components"],
+  // react router dom
+  router: ["react-router-dom"],
+  // axios
+  axios: ["axios"],
+  // tanstack query
+  query: ["@tanstack/react-query"],
+  // react
+  react: ["react", "react-dom", "react-scripts"],
+};
+
+module.exports = packagesDependencies;
